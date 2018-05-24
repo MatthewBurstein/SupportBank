c5bb9ba3dc96d7c9084a5186310df12fb2ecbc62
Matt Burstein <matthew.burstein@softwire.com>
2018-05-24 14:20:41 +0100

    feat: Add account class

---
+++ src/main/java/training/supportbank/Account.java
@@ -0,0 +1,13 @@
+package training.supportbank;
+
+public class Account {
+
+    private final String name;
+    private final Float balance;
+
+    public Account(String name) {
+        this.name = name;
+        this.balance = Float.valueOf(0);
+    }
+
+}
